382a42fb3f987504cd34f165614c79a9008fa401
pigeon79 <pigeon79@163.com>
2019-03-04 13:42:39 +0800

    添加项目文件。

---
+++ HYWebApp/HYWebApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using HYWebApp.ServiceModel;
+
+namespace HYWebApp.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
